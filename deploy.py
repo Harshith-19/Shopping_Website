55e7cb6c9b6aff24dbeb230df84c605c4bccf6e1
hemant gaur <hemantgaur@iitbhilai.ac.in>
2022-03-08 22:24:23 +0530

    css, python, html part of policy get commited

--- deploy.py
+++ deploy.py
@@ -2,5 +2,10 @@ import pickle
 import json
 from flask import Flask, render_template, request, jsonify   
 
+
+@app.route("/policy")
+def policy():
+    return render_template("policy.html")
+
 if __name__=="__main__":
     app.run()
