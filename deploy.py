9fe4450a45e0f27eb503affe176629b5eb2e1751
Hrishik Kanade <hrishikrajeshkanade@iitbhilai.ac.in>
2022-03-09 01:24:06 +0600

    Cherry-pick done

--- deploy.py
+++ deploy.py
@@ -10,5 +10,17 @@ def history():
         c+=i
     return render_template("history.html",c=c,P=P,d=d)
     
+<<<<<<< HEAD
+=======
+
+@app.route("/policy")
+def policy():
+    return render_template("policy.html")
+  
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+>>>>>>> 76deb00 (New feature Integration)
 if __name__=="__main__":
     app.run()
