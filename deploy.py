70ce7491073d04bb2324da9456bbe74ac0e9716d
Harshith <harshith.jethu@gmail.com>
2022-03-08 22:49:33 +0530

    History feature is added

--- deploy.py
+++ deploy.py
@@ -2,5 +2,13 @@ import pickle
 import json
 from flask import Flask, render_template, request, jsonify   
 
+@app.route("/history")
+def history():
+    c=0
+    d=0
+    for i in P:
+        c+=i
+    return render_template("history.html",c=c,P=P,d=d)
+    
 if __name__=="__main__":
     app.run()
